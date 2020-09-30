783be815e1c360ecc9a8955e4bfbeddd716ecdde
jvail <6503378+jvail@users.noreply.github.com>
2020-09-30 12:05:21 +0400

    add sysroot

--- conda/build.sh
+++ conda/build.sh
@@ -16,10 +16,11 @@ if [ `uname` = "Darwin" ]; then
     xcrun --show-sdk-path
     ls /opt
 else
-    SYSTEM_DEPENDENT_ARGS=(
-        "-DOPENGL_opengl_LIBRARY=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so"
-        "-DOPENGL_glx_LIBRARY=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so"
-    )
+SYSTEM_DEPENDENT_ARGS=(
+    "-DOPENGL_opengl_LIBRARY=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so"
+    "-DOPENGL_glx_LIBRARY=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so"
+    "-DOPENGL_INCLUDE_DIR=${BUILD_PREFIX}/${HOST}/sysroot/usr/include"
+)
 fi
 
 export SYSTEM_DEPENDENT_ARGS
@@ -42,7 +43,7 @@ cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_BUILD_TYPE=Release  \
       -DPython3_EXECUTABLE=${PYTHON} \
        ${SYSTEM_DEPENDENT_ARGS[@]} \
-      -LAH .. 
+      -LAH ..
 
 echo
 echo "****** PGL CONFIG"
@@ -51,7 +52,7 @@ cat $SRC_DIR/src/cpp/plantgl/userconfig.h
 echo
 echo "****** COMPILE"
 export VERBOSE=1
-make -j${CPU_COUNT} 
+make -j${CPU_COUNT}
 echo "****** INSTALL CXX LIB"
 make install
 
@@ -62,7 +63,7 @@ echo "PYTHON:" ${PYTHON}
 
 #echo "** PYTHON CALL"
 #export PYTHONPATH=${PREFIX}/lib/python${PY_VER}/site-packages/
-${PYTHON} setup.py install --prefix=${PREFIX} 
+${PYTHON} setup.py install --prefix=${PREFIX}
 
 echo
 echo "****** CHECK PYTHON LIB"
@@ -71,7 +72,7 @@ echo "****** CHECK PYTHON LIB"
 if [ `uname` = "Darwin" ]; then
     export LDD='otool -L'
 else
-    export LDD='ldd'    
+    export LDD='ldd'
 fi
 
 ${LDD} `${PYTHON} -c "import openalea.plantgl.math._pglmath as pm ; print(pm.__file__)"`
